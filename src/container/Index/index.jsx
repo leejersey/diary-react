c47a0560bb633985a891301915983f987530e939
lizexi <Zexi.Li@b-and-qchina.com>
2021-08-09 17:07:21 +0800

    ✨ feat(新增组件库):

---
+++ src/container/Index/index.jsx
@@ -0,0 +1,10 @@
+// Index/index.jsx
+import React from 'react'
+import { Button } from 'zarm'
+
+export default function Index() {
+  return <div>
+    Index
+    <Button theme='primary'>按钮</Button>
+  </div>
+}
