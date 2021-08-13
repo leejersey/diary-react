b33ba5984fc7e12de390f1fb3fb6550ae887aee9
jersey <lizexi711@gmail.com>
2021-08-13 10:28:35 +0800

    ✨ feat(完成账单类型弹窗):

---
+++ src/components/PopupType/index.jsx
@@ -0,0 +1,74 @@
+// 弹出层组件
+import React, { useState, useEffect, forwardRef } from 'react'
+import { Popup, Icon } from 'zarm';
+import { get } from '@/utils'
+import cx from 'classnames';
+
+import s from './style.module.less'
+
+// 重点：forwardRef拿到父组件传入的ref熟悉，这样父组件就能通过ref控制子组件
+const PopupType = forwardRef(( {onSelect}, ref) => {
+    const [show, setShow] = useState(false)
+    const [active, setActive] = useState('all'); // 激活的 type
+    const [expense, setExpense] = useState([])
+    const [income, setIncome] = useState([])
+    // 获取类型列表数据
+    useEffect(async () => {
+        const { data: { list } } = await get('/api/type/list');
+        setExpense(list.filter(i => i.type == 1))
+        setIncome(list.filter(i => i.type == 2))
+    }, [])
+
+    if(ref){
+        ref.current = {
+            show: () => {
+                setShow(true)
+            },
+            close: () => {
+                setShow(false)
+            }
+        }
+    }
+
+    const choseType = (item) => {
+        console.log(item)
+        setActive(item.id);
+        setShow(false)
+        // 传值到父组件
+        onSelect(item);
+    }
+
+    return (
+        <Popup
+        visible={show}
+        direction="bottom"
+        onMaskClick={() => setShow(false)}
+        destroy={false}
+        mountContainer={() => document.body}
+      >
+        <div className={s.popupType}>
+            <div className={s.header}>
+                请选择类型
+                <Icon type="wrong" className={s.cross} onClick={() => setShow(false)} />
+            </div>
+            <div className={s.content}>
+                <div onClick={() => choseType({ id: 'all' })} className={cx({ [s.all]: true, [s.active]: active == 'all' })}>全部类型</div>
+                <div className={s.title}>支出</div>
+                <div className={s.expenseWrap}>
+                {
+                    expense.map((item, index) => <p key={index} onClick={() => choseType(item)} className={cx({[s.active]: active == item.id})} >{ item.name }</p>)
+                }
+                </div>
+                <div className={s.title}>收入</div>
+                <div className={s.incomeWrap}>
+                {
+                    income.map((item, index) => <p key={index} onClick={() => choseType(item)} className={cx({[s.active]: active == item.id})} >{ item.name }</p>)
+                }
+                </div>
+            </div>
+        </div>
+      </Popup>
+    )
+})
+
+export default PopupType;
--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Icon, Pull } from 'zarm'
 import dayjs from 'dayjs'
 import BillItem from '@/components/BillItem'
 import { get, REFRESH_STATE, LOAD_STATE } from '@/utils'
-
+import PopupType from '@/components/PopupType'
 import s from './style.module.less'
 
 const Home = () => {
+    const typeRef = useRef();
+    const [currentSelect, setCurrentSelect] = useState({}); // 当前筛选类型
     const [currentTime, setCurrentTime] = useState(dayjs().format('YYYY-MM'))
     const [page, setPage] = useState(1); // 分页
     const [list, setList] = useState([])
@@ -16,10 +18,10 @@ const Home = () => {
 
     useEffect(() => {
         getList()
-    }, [page])
+    }, [page, currentSelect])
 
     const getList = async () => {
-        const { data } = await get(`/api/bill/list?page=${page}&page_size=5&date=${currentTime}`)
+        const { data } = await get(`/api/bill/list?page=${page}&page_size=5&date=${currentTime}&type_id=${currentSelect.id || 'all'}`)
         
         // 下拉刷新
         if(page == 1){
@@ -50,6 +52,18 @@ const Home = () => {
           }
     }
 
+    const toggle = () => {
+        typeRef.current && typeRef.current.show()
+    }
+
+    const select = (item) => {
+        console.log(item);
+        setRefreshing(REFRESH_STATE.loading);
+        // 触发刷新列表，将分页重制为 1
+        setPage(1);
+        setCurrentSelect(item)
+    }
+
     return (
         <div className={s.home}>
             <div className={s.header}>
@@ -58,8 +72,8 @@ const Home = () => {
                 <span className={s.income}>总收入：<b>¥ 500</b></span>
                 </div>
                 <div className={s.typeWrap}>
-                <div className={s.left}>
-                    <span className={s.title}>类型 <Icon className={s.arrow} type="arrow-bottom" /></span>
+                <div className={s.left} onClick={toggle}>
+                    <span className={s.title}>{ currentSelect.name || '全部类型' }  <Icon className={s.arrow} type="arrow-bottom" /></span>
                 </div>
                 <div className={s.right}>
                     <span className={s.time}>2022-06<Icon className={s.arrow} type="arrow-bottom" /></span>
@@ -88,6 +102,7 @@ const Home = () => {
                 
             }
             </div>
+            <PopupType ref={typeRef} onSelect={select} />
         </div>
     )
 }
